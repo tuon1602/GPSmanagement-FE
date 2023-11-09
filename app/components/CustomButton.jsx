f6d0abb4bbcdfa2daf833ba7cfce7d7ab36a2c75
nguyen tuan <nguyen tuan>
2023-11-09 18:11:37 +0700

    fix

---
+++ app/components/CustomButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@nextui-org/button'
+
+const CustomButton = (props) => {
+  return (
+    <Button>{props.title}</Button>
+  )
+}
+
+export default CustomButton
